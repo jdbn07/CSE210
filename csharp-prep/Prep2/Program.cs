452be42cf40d08f120b629ee4538630ef035e8ca
jdbn07 <jdbn_07@hotmail.com>
2024-04-24 23:11:58 -0500

    prep2 resolved

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your qualification? ");
+        string userqua  = Console.ReadLine();
+        int qua = int.Parse(userqua);
+        string letter="";
+
+        if (qua >= 90)
+        {
+            letter = "A"; 
+        }
+        else if (qua <= 89 && qua>=80)
+        {
+           letter = "B"; 
+        }
+        else if (qua <= 79 && qua>=70)
+        {
+           letter = "C";
+        }
+        else if (qua <= 69 && qua>=60)
+        {
+           letter = "D";
+        }
+        else
+        {
+           letter = "f";
+        }
+        Console.WriteLine ("Your Grade is: "+letter);
+        if ( qua >=70)
+        {
+            Console. WriteLine ("Congratulations, you passed");
+        }
+        else
+        {
+            Console.WriteLine ("Good Luck Next Time, Try Again");
+        }
     }
 }
